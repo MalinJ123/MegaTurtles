50410af3e0a9bd160bb9dbb173b8c6303a25687a
MalinJ123 <malin--jonsson@hotmail.com>
2023-04-04 12:24:33 +0200

    menu

--- MegaTurtles/src/App.jsx
+++ MegaTurtles/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import MenuPage from './Components/MenuPage'
 
 function App() {
  
   return (
     <div className="App">
     
+    <MenuPage/>
     </div>
   )
 }
