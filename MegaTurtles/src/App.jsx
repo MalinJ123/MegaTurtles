468f3b85d347f832781682e502c9d29b528df475
AnsoMalm <112514545+AnsoMalm@users.noreply.github.com>
2023-04-04 14:14:51 +0200

    added new files

--- MegaTurtles/src/App.jsx
+++ MegaTurtles/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import ContentMain from './Components.context.jsx'
+import ContentMain from './Components/content'
 
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <ContentMain />
+
     </div>
   )
 }
--- MegaTurtles/src/Components/content.jsx
+++ MegaTurtles/src/Components/content.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react"
+import middagsbord from '..assets/imgs/middagsbord'
 
 const ContentMain = () => {
+
 	return (
-		<div className="main-container">
-			<img></img>
+		<div>
+		<h1>Välkommen till oss på </h1>
+		<img scr={middagsbord} alt="middagsbord" />
 			<p></p>
 		</div>
 
 	)
+	
 }
 
 export default ContentMain
