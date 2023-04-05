a4e24d985ae0c98f64e51b0e9b94b5f20e02627d
AnsoMalm <112514545+AnsoMalm@users.noreply.github.com>
2023-04-05 11:58:35 +0200

    added new media querys and fix the layout for the app and webside

--- MegaTurtles/src/Components/content.jsx
+++ MegaTurtles/src/Components/content.jsx
@@ -5,18 +5,26 @@ import './ContentMain.css'
 const ContentMain = () => {
 
 	return (
+		<main>
 		<div className="main-container">
-			<h1>Välkommen till oss!</h1>
-				<img className="glasbord" src={bord} />
-					<h2>Är du på jakt efter en riktig bra restaurang som erbjuder den bästa servicen och smakupplevelsen? <br/> Då har du kommit rätt! </h2>
-
-					<p>Middagshörnan är det perfekta valet om du är ute efter god mat och ett brett utbud av drycker och matalternativ till fantastiskt bra priser.  </p>
-
-					<p>Vi är en nyöppnad och mysig, liten restaurang som vill ge dig som gäst den bästa servicen och smaksensation ifrån vår nya och fräscha meny.</p> 
-						
-					<p>Du som gäst får även tycka till om vad du tycker om vår mat och ifall du har egna ideér på nya rätter, tveka inte att höra av dig till oss.</p>
+			<img className="glasbord" src={bord} />
+				<div className="text-container">
+					<div className="main-text">
+						<h2>Är du på jakt efter en riktig bra restaurang som erbjuder den bästa servicen och smakupplevelsen? <br/> Då har du kommit rätt! </h2>
+					</div>
+					<div className="main-para-container">
+						<p className="main-para">Middagshörnan är det perfekta valet om du är ute efter god mat och ett brett utbud av drycker och matalternativ till fantastiskt bra priser.  </p>
+					</div>
+					<div className="main-para-container">
+						<p className="main-para">Vi är en nyöppnad och mysig, liten restaurang som vill ge dig som gäst den bästa servicen och smaksensation ifrån vår nya och fräscha meny.</p> 
+					</div>
+					<div className="main-para-container">
+						<p className="main-para">Du som gäst får även tycka till om vad du tycker om vår mat och ifall du har egna ideér på nya rätter, tveka inte att höra av dig till oss.</p>
+					</div>	
+			</div>
+				
 		</div>
-
+	</main>
 	)
 	
 }
