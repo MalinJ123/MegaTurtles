b5ff151069344c8c876447bd740c873f7abfd7da
Linn Gadevik <gadeviklinn@gmail.com>
2023-04-05 14:23:45 +0200

    Footer klar enligt mig

--- MegaTurtles/src/App.jsx
+++ MegaTurtles/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Footer from './Components/footer.jsx/'
+import './footer.css'
 
 function App() {
  
   return (
     <div className="App">
-    
+      <Footer />
     </div>
   )
 }
