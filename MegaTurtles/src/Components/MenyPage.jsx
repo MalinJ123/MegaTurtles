bc7db08e0262f945f1db828a11755e8f0b317220
MalinJ123 <malin--jonsson@hotmail.com>
2023-04-05 11:59:14 +0200

    menuorder style

--- MegaTurtles/src/Components/MenyPage.jsx
+++ MegaTurtles/src/Components/MenyPage.jsx
@@ -1,47 +1,52 @@
 import React from "react";
 import Fish from '../assets/imgs/Paneradfisk.jpg';
+import Kött from '../assets/Imgs/Kött.jpg';
+import kyckling from '../assets/Imgs/kyckling.jpg';
 import '../Stylesheet/menyPage.css'
 
 
 function MenyPage() {
 
-return (
-	
-	<>
-		<section className="menySection"> 
+	const matratter = [
+	  {
+		namn: "Rödspätta",
+		bild: Fish,
+		beskrivning: "Rödspätta, potatis, ärtor, remouladsås, citronskiva"
+	  },
+	  {
+		namn: "Lammstek",
+		bild: Kött,
+		beskrivning: "Lammstek med potatisgratäng och bearnaisesås"
+	  },
+	  {
+		namn: "Grillad Kyckling",
+		bild: kyckling,
+		beskrivning: "Grillad kyckling med pommens, sallad, tomater och"
+	  }
+	];
+  
+	return (
+	  <>
+		<div className="menySection">
 			<h3 className="meny-h3">Meny</h3>
+		</div>
+		{matratter.map((matratt, index) => (
+		  <section className="menySection" key={index}>
 			<p>______________________________</p>
-			<h4>Panerad rödspätta med kokt potatis</h4>
-			<img className="meny-pic" src={Fish} alt="Beskrivning av din bild" />
-			<p>Rödspätta, potatis, ärtor, remouladsås, citronskiva </p>
-			<button className="läggtill-Btn">+</button>
-	
-		</section>
-
-		<section className="menySection"> 
+			<h4>{matratt.namn}</h4>
+			<img className="meny-pic" src={matratt.bild} alt="Beskrivning av din bild" />	
 			
-			<p>______________________________</p>
-			<h4>Panerad rödspätta med kokt potatis</h4>
-			<img className="meny-pic" src={Fish} alt="Beskrivning av din bild" />
-			<p>Rödspätta, potatis, ärtor, remouladsås, citronskiva </p>
+			<section className="beskrivningP">
+				<p>{matratt.beskrivning}</p>
+			</section>
 			<button className="läggtill-Btn">+</button>
-
-		</section>
-
-		<section className="menySection"> 
+		  </section>
 			
-			<p>______________________________</p>
-			<h4>Panerad rödspätta med kokt potatis</h4>
-			<img className="meny-pic" src={Fish} alt="Beskrivning av din bild" />
-			<p>Rödspätta, potatis, ärtor, remouladsås, citronskiva </p>
-			<button className="läggtill-Btn">+</button>
-
-		</section>
-	</>
-	)
-	 
+		))}
+	  </>
+	);
   }
-	
+  
 
   
 
