a5908d0ccef58f668bdb599a9c4f19212823dc23
MalinJ123 <malin--jonsson@hotmail.com>
2023-04-05 10:02:47 +0200

    meny more orders

--- MegaTurtles/src/Components/MenyPage.jsx
+++ MegaTurtles/src/Components/MenyPage.jsx
@@ -7,15 +7,37 @@ function MenyPage() {
 
 return (
 	
+	<>
 		<section className="menySection"> 
 			<h3 className="meny-h3">Meny</h3>
 			<p>______________________________</p>
 			<h4>Panerad rödspätta med kokt potatis</h4>
 			<img className="meny-pic" src={Fish} alt="Beskrivning av din bild" />
 			<p>Rödspätta, potatis, ärtor, remouladsås, citronskiva </p>
-			<div><button className="läggtill-Btn">+</button>
-			<button className="tabort-Btn">-</button></div>
+			<button className="läggtill-Btn">+</button>
+	
 		</section>
+
+		<section className="menySection"> 
+			
+			<p>______________________________</p>
+			<h4>Panerad rödspätta med kokt potatis</h4>
+			<img className="meny-pic" src={Fish} alt="Beskrivning av din bild" />
+			<p>Rödspätta, potatis, ärtor, remouladsås, citronskiva </p>
+			<button className="läggtill-Btn">+</button>
+
+		</section>
+
+		<section className="menySection"> 
+			
+			<p>______________________________</p>
+			<h4>Panerad rödspätta med kokt potatis</h4>
+			<img className="meny-pic" src={Fish} alt="Beskrivning av din bild" />
+			<p>Rödspätta, potatis, ärtor, remouladsås, citronskiva </p>
+			<button className="läggtill-Btn">+</button>
+
+		</section>
+	</>
 	)
 	 
   }
