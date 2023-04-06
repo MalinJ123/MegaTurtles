7e3d4f8a5507476906020c6b14b54837eab917da
Therese <thereseskold@live.com>
2023-04-06 09:42:35 +0200

    admin

--- MegaTurtles/src/Components/adminlogin.jsx
+++ MegaTurtles/src/Components/adminlogin.jsx
@@ -1,12 +1,16 @@
 const AdminLogin = () => {
+
+	const checkPassword = ()=> {
+		
+	}
 	
 	return (
 		<section className="overlay">
 			<div className="form-container">
 				<h2>Logga in</h2>
-				<input className="input" type= "number" placeholder= "Personalnummer"></input>
+				<input className="input" type= "text" placeholder= "Admin"></input>
 				<input className="input" type= "text" placeholder= "Lösenord"></input>
-				<button>Logga in</button>
+				<button onClick={checkPassword}>Logga in</button>
 			</div>
 		</section>
 	)
