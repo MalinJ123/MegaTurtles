39752419e8c45b3f27552d7dfbc1502c8a4b270e
Therese <thereseskold@live.com>
2023-04-11 13:54:05 +0200

    Flyttade ut datan till menyn i en egen fil och mapp

--- MegaTurtles/src/Components/MenyPage.jsx
+++ MegaTurtles/src/Components/MenyPage.jsx
@@ -1,10 +1,6 @@
 import React, { useState } from "react";
-import panerad from '../assets/imgs/panerad.jpg';
-import Lammstek from '../assets/Imgs/lammstek.jpg';
-import kyckling from '../assets/Imgs/kyckling.jpg';
-import Entrecote from '../assets/Imgs/Entrecote.jpg';
-import sallad from '../assets/Imgs/sallad.jpg';
 import '../Stylesheet/menyPage.css';
+import { menu } from "../data/menudata";
 
 const MenyPage = ({ cartItems, setCartItems, addToCart }) => {
   const [clickCount, setClickCount] = useState(0);
@@ -14,38 +10,7 @@ const MenyPage = ({ cartItems, setCartItems, addToCart }) => {
     setClickCount(clickCount + 1);
   };
 
-  const menu = [
-    {
-      namn: "Rödspätta",
-      bild: panerad,
-      beskrivning: "Panerad rödspätta som serveras med potatis, citronsås och ärtor",
-      price: 125,
-    },
-    {
-      namn: "Lammstek",
-      bild: Lammstek,
-      beskrivning: "Lammstek med potatisgratäng och rödvinssås och sparris",
-      price: 125 ,
-    },
-    {
-      namn: "Kyckling",
-      bild: kyckling,
-      beskrivning: "Grillad kyckling med pommens, sallad, tomater och kärlek",
-      price: 149,
-    },
-    {
-      namn: "Entrecote",
-      bild: Entrecote,
-      beskrivning: "Saftig Entrecote med pommes och bearnaisesås",
-      price: 225,
-    },
-    {
-      namn: "XL Sallad",
-      bild: sallad,
-      beskrivning: "Saftig sallad med inslag av natur. ",
-      price: 105
-    }
-  ];
+  
 
   return (
     <>
