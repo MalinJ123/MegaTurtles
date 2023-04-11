9994fcde96d919423c6be199c3a146acfc32f4c0
Therese <thereseskold@live.com>
2023-04-11 15:24:12 +0200

    fixade så att felmeddelandet kommer när man skrivit in och att det försvinner när det blir tomt

--- MegaTurtles/src/Components/adminlogin.jsx
+++ MegaTurtles/src/Components/adminlogin.jsx
@@ -8,16 +8,29 @@ const AdminLogin = (props) => {
 	const [password, setPassword] = useState('')
 	const [isPasswordCorrect, setIsPasswordCorrect] = useState(false);
 	const [hasSubmitted, setHasSubmitted] = useState(false)
+	const [showAdminPage, setShowAdminPage] = useState(false)
 	const correctPassword = 'Mums'
 
-	const checkPassword = ()=> {
+	const LoginEvents = (event)=> {
+		event.preventDefault()
 		setHasSubmitted(true)
+
 		if (password === correctPassword) {
 			setIsPasswordCorrect(true)
+			setShowAdminPage(true)
 		} else {
 			setIsPasswordCorrect(false)
 		}
 		
+	}
+
+	// Nu vill jag skapa en funktion där jag skickas vidare till adminsidan som ska ligga i funktionen LoginEvent. Jag tror att jag ska börja med att skapa en statevariabel för att visa den sidan.
+
+	const handleInputChange = (event) => {
+		setPassword(event.target.value)
+		if (hasSubmitted) {
+			setIsPasswordCorrect(event.target.value === correctPassword)
+		} 
 	}
 
 	const closeLogin = () => {
@@ -25,19 +38,39 @@ const AdminLogin = (props) => {
 		props.onClose();
 	} 
 
+	
+
 
 	
 	return (
 		<section className="overlay" style={{ display: close ? 'none' : 'fixed' }}>
-			<div className="form-container">
+			<form className="form-container">
 				<FontAwesomeIcon onClick={closeLogin} className= 'close-icon-admin' icon={faXmark} />
 				
 				<h2 className="login">Logga in</h2>
-				<input className="input" type= "text" placeholder= "Admin"></input>
-				{hasSubmitted && !isPasswordCorrect && <p>Fel lösenord</p>}
-				<input className="input" type= "text" placeholder= "Lösenord"></input>
-				<button onClick={checkPassword}>Logga in</button>
-			</div>
+				<label className='label-style'>Avändarnamn: </label>
+				<input 
+					className="input" 
+					type= "text" 
+					placeholder= "Admin">
+				</input>
+
+				<label className='label-style'>Lösenord:</label>
+				<input 
+					className="input" 
+					type= "password" 
+					placeholder= "Lösenord"
+					value={password}
+					onChange={handleInputChange}>
+				</input>
+					{hasSubmitted && !isPasswordCorrect && password !== '' && <p className='error-message'>Fel lösenord</p> }
+
+				<button 
+					type="submit" 
+					onClick={LoginEvents}> Logga in
+				</button>
+
+			</form>
 		</section>
 	)
 }
