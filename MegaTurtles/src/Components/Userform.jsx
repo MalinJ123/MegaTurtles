a7268ed894bafa9ec4b8cea4871ed7af6495511f
AnsoMalm <112514545+AnsoMalm@users.noreply.github.com>
2023-04-12 16:06:19 +0200

    fixed valid on name and started on tel in forms

--- MegaTurtles/src/Components/Userform.jsx
+++ MegaTurtles/src/Components/Userform.jsx
@@ -1,20 +1,29 @@
 import { useState } from "react"
 import '../Stylesheet/userForm.css'
 import contactUs from '../assets/imgs/contactus.jpg'
-import { isValidFullName } from "../utils/vaildator";
+import { isValidFullName, isValidTelephone } from "../utils/vaildator.js";
+
 
 
 function UserForm() {
 	const [name, setName] = useState('');
 	const [nameIsDirty, setNameIsDirty] = useState(false);
 	const [tel, setTel] = useState('');
+	const [telIsDirty, setTelIsDirty] = useState(false); 
 	const [message, setMessage] = useState('')
 
 //required - Glöm ej det
 //min max - telefonnummer type: tel
-//
 
 const [nameIsValid, nameErrorMessage ]= isValidFullName(name)
+const [telIsvalid, teleErrorMessage] = isValidTelephone(tel)
+
+//Namn input
+const handleNameChange = (e) => setName(e.target.value)
+const formNameinput = nameIsDirty ? (nameIsValid ? 'valid' : 'invalid') : ''
+
+//Telefon input
+const handleTelephoneChange = (e) => setTel(e.target.value);
 
 const handleSubmit = event => {
 	event.preventDefault()
@@ -24,31 +33,40 @@ const handleSubmit = event => {
 	<section className="contact">
 		<form className="user-form">
 				<p className="user-form-header">Har du ideér till en ny rätt eller vill fråga oss om något? Vänligen skriv till oss så kanske vi uppdaterar våran meny med just DIN mat</p>
+
 				<div className="field-group">
-				 <label className="name-input"> NAMN: </label>
-					<input className="form-input " 
-					type="text" 
-					value={name} 
-					onChange={(e) => setName(e.target.value)}
-					onBlur={() => setNameIsDirty(true)} required
-					/>
-					<span>{nameIsDirty ? nameIsValid ? '✔️' : '❌' : ''} </span>
+				 <label className="name-input"> NAMN: </label>	
+					<div className="field">
+						<input className={formNameinput}
+						type="text" 
+						value={name} 
+						onChange={handleNameChange}
+						onBlur={() => setNameIsDirty(true)}
+						/>
+						<span>{nameIsDirty ? (nameIsValid ? '✔️' : '❌') : '' }</span>
+					</div>
+						<span className="display-error-message">{nameIsDirty ? nameErrorMessage : ''}</span>
 				</div>
+
 				<div className="field-group">
-				<label className="number-input">
-					TELEFONNUMMER: </label>
-					<input className="form-input" type="tel" 
-					value={tel} 
-					onChange={(e) => setTel(e.target.value)} required/>	
+					<label className="number-input">TELEFONNUMMER: </label>
+						<div className="field">
+							<input 
+							className="form-input" 
+							type="tel" 
+							value={tel} 
+							onChange={handleTelephoneChange}
+							onBlur={() => setTelIsDirty(true)}/>	
+						</div>
 				</div>	
+
 				<div className="field-group">
-					<label className="message-container">
-						MEDDELANDE: </label>
-							<textarea className="message-box" type="text" 
-							value={message}
-							onChange={(e) => setMessage(e.target.value)} 
-							rows="10" column="100"
-							minlenght="10" maxlenght="100" placeholder="Lämna meddelande till restaurangen"></textarea>
+					<label className="message-container"> MEDDELANDE: </label>
+						<textarea className="message-box" type="text" 
+						value={message}
+						onChange={(e) => setMessage(e.target.value)} 
+						rows="10" column="100"
+						minlenght="10" maxlenght="100" placeholder="Lämna meddelande till restaurangen"></textarea>
 				</div>
 			
 				<button className="form-button" type="submit" onClick={handleSubmit}>Skicka in!</button>
--- MegaTurtles/src/utils/vaildator.js
+++ MegaTurtles/src/utils/vaildator.js
@@ -1,20 +1,32 @@
 function isValidFullName(fullName) {
-	if( fullName.length < 5 ) {
+	if (fullName.length < 5 ) {
 		return [false, 'Minst 5 tecken tack.']; 
 	}
-	if( !fullName.includes(' ') ) {
+	if (!fullName.includes(' ')) {
 		return [false, 'Vänligen fyll i både för- och efternamn.']; 
 	}
-
-	//regex
-	const allowChars = 'abcdefghijklmnopqrstuvwxyzåäö'
-	for(i=0; i<fullName.length; i++) {
+	const allowChars = ' abcdefghijklmnopqrstuvwxyzåäö'
+	for(let i = 0; i < fullName.length; i++) {
 		let c = fullName.charAt(i).toLowerCase()
 		if(!allowChars.includes(c) ) {
-			return [false, 'Vänligen använd bara svenska bokstäver']; 
+			return [false, 'Vänligen använd bara bokstäver']; 
 		}
 	}
 	return [true, '']; 
 }
 
-export { isValidFullName }
+function isValidTelephone(fullPhoneNumber) {
+	if(fullPhoneNumber.length < 10) {
+		return [false, 'Minst 10 siffror tack.'];
+	}
+	const allowNumber = '0, 1, 2, 3, 4, 5, 6, 7, 8, 9'
+	for(let i = 0; i < fullPhoneNumber.length; i++) {
+		let n = fullPhoneNumber.charAt(i)
+		if(!allowNumber.includes(n) ) {
+			return [false, 'Vänligen använd bara siffror'];
+		}
+	}
+	return [true, '']
+}
+
+export { isValidFullName, isValidTelephone }
