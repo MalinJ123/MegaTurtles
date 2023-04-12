0d2acc32d45ff37100e2f18eaeaf7dd88cf0b0d7
MalinJ123 <malin--jonsson@hotmail.com>
2023-04-12 12:17:17 +0200

    dev

--- MegaTurtles/src/Components/MenyPage.jsx
+++ MegaTurtles/src/Components/MenyPage.jsx
@@ -1,13 +1,4 @@
 import React, { useState } from "react";
-<<<<<<< HEAD
-import panerad from '../assets/imgs/panerad.jpg';
-import Lammstek from '../assets/Imgs/lammstek.jpg';
-import kyckling from '../assets/Imgs/kyckling.jpg';
-import Entrecote from '../assets/Imgs/Entrecote.jpg';
-import sallad from '../assets/Imgs/sallad.jpg';
-import Drycker from '../assets/imgs/Drycker.jpg'
-=======
->>>>>>> 39752419e8c45b3f27552d7dfbc1502c8a4b270e
 import '../Stylesheet/menyPage.css';
 import { menu } from "../data/menudata";
 
@@ -19,48 +10,7 @@ const MenyPage = ({ cartItems, setCartItems, addToCart }) => {
     setClickCount(clickCount + 1);
   };
 
-<<<<<<< HEAD
-  const menu = [
-    {
-      namn: "Rödspätta",
-      bild: panerad,
-      beskrivning: "Panerad rödspätta som serveras med potatis, citronsås och ärtor",
-      price: 125,
-    },
-    {
-      namn: "Lammstek",
-      bild: Lammstek,
-      beskrivning: "Lammstek med potatisgratäng och rödvinssås och sparris",
-      price: 125 ,
-    },
-    {
-      namn: "Kyckling",
-      bild: kyckling,
-      beskrivning: "Grillad kyckling med pommens, sallad, tomater och kärlek",
-      price: 149,
-    },
-    {
-      namn: "Entrecote",
-      bild: Entrecote,
-      beskrivning: "Saftig Entrecote med pommes och bearnaisesås",
-      price: 225,
-    },
-    {
-      namn: "XL Sallad",
-      bild: sallad,
-      beskrivning: "Saftig sallad med inslag av natur. ",
-      price: 105
-    },
-    {
-      namn: "Drycker",
-      bild: Drycker,
-      beskrivning: "Vatten, Zingo, Trocadero, Sprite, Coca Cola, Fanta ",
-      price: 15
-    }
-  ];
-=======
   
->>>>>>> 39752419e8c45b3f27552d7dfbc1502c8a4b270e
 
   return (
     <>
