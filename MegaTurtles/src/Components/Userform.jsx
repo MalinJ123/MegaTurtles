ccdb165f7a87933324111785e07634fd1ad52325
AnsoMalm <112514545+AnsoMalm@users.noreply.github.com>
2023-04-13 12:07:24 +0200

    added new function and reactcode to message-textarea in inputfield for user

--- MegaTurtles/src/Components/Userform.jsx
+++ MegaTurtles/src/Components/Userform.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react"
 import '../Stylesheet/userForm.css'
 import contactUs from '../assets/imgs/contactus.jpg'
-import { isValidFullName, isValidTelephone } from "../utils/vaildator.js";
-
+import { isValidFullName, isValidTelephone, isValidMessage } from "../utils/vaildator.js";
 
 
 function UserForm() {
@@ -11,12 +10,13 @@ function UserForm() {
 	const [tel, setTel] = useState('');
 	const [telIsDirty, setTelIsDirty] = useState(false); 
 	const [message, setMessage] = useState('')
+	const [messageIsDirty, setMessageIsDirty] = useState(false);
 
 //required - Glöm ej det
-//min max - telefonnummer type: tel
 
 const [nameIsValid, nameErrorMessage ]= isValidFullName(name)
 const [telIsvalid, teleErrorMessage] = isValidTelephone(tel)
+const [textIsValid, textErrorMessage] = isValidMessage(message)
 
 //Namn input
 const handleNameChange = (e) => setName(e.target.value)
@@ -24,6 +24,21 @@ const formNameinput = nameIsDirty ? (nameIsValid ? 'valid' : 'invalid') : ''
 
 //Telefon input
 const handleTelephoneChange = (e) => setTel(e.target.value);
+const formTelePhoneinput = telIsDirty ? (telIsvalid ? 'valid' : 'invalid') : ''
+
+//Message input
+const handleMessageChange = (e) => {
+	let input = e.target.value;
+	console.log(e.target.value);
+	if (input.length > 100 || input.length < 10) {
+		setMessageIsDirty(true)
+		console.log(input.length);
+	} else {
+		setMessageIsDirty(false)
+	}
+	setMessage(e.target.value)
+}; 
+const formMessageInput = messageIsDirty ? (textIsValid ? 'valid' : 'invalid') : ''
 
 const handleSubmit = event => {
 	event.preventDefault()
@@ -48,25 +63,30 @@ const handleSubmit = event => {
 						<span className="display-error-message">{nameIsDirty ? nameErrorMessage : ''}</span>
 				</div>
 
-				<div className="field-group">
+				 <div className="field-group">
 					<label className="number-input">TELEFONNUMMER: </label>
 						<div className="field">
 							<input 
-							className="form-input" 
+							className={formTelePhoneinput} 
 							type="tel" 
 							value={tel} 
 							onChange={handleTelephoneChange}
-							onBlur={() => setTelIsDirty(true)}/>	
+							onBlur={() => setTelIsDirty(true)}/>
+							<span>{telIsDirty ? (telIsvalid ? '✔️' : '❌') : '' }</span>
 						</div>
-				</div>	
+						<span className="display-error-phone- message">{telIsDirty ? teleErrorMessage : '' }</span>
+				</div>	 
 
 				<div className="field-group">
 					<label className="message-container"> MEDDELANDE: </label>
 						<textarea className="message-box" type="text" 
 						value={message}
-						onChange={(e) => setMessage(e.target.value)} 
+						onChange={handleMessageChange} 
 						rows="10" column="100"
-						minlenght="10" maxlenght="100" placeholder="Lämna meddelande till restaurangen"></textarea>
+						minlenght="10" maxlenght="100" 
+						placeholder="Lämna meddelande till restaurangen">
+						</textarea>
+						<span className="display-error-text-message">{messageIsDirty ? textErrorMessage : '' }</span>
 				</div>
 			
 				<button className="form-button" type="submit" onClick={handleSubmit}>Skicka in!</button>
--- MegaTurtles/src/utils/vaildator.js
+++ MegaTurtles/src/utils/vaildator.js
@@ -1,5 +1,7 @@
+
+//Funktionen för namn-input
 function isValidFullName(fullName) {
-	if (fullName.length < 5 ) {
+	if (fullName.length < 6 ) {
 		return [false, 'Minst 5 tecken tack.']; 
 	}
 	if (!fullName.includes(' ')) {
@@ -14,19 +16,33 @@ function isValidFullName(fullName) {
 	}
 	return [true, '']; 
 }
-
+//Funktionen för telefon-input
 function isValidTelephone(fullPhoneNumber) {
 	if(fullPhoneNumber.length < 10) {
 		return [false, 'Minst 10 siffror tack.'];
 	}
-	const allowNumber = '0, 1, 2, 3, 4, 5, 6, 7, 8, 9'
+	const allowNum = '0, 1, 2, 3, 4, 5, 6, 7, 8, 9'
 	for(let i = 0; i < fullPhoneNumber.length; i++) {
 		let n = fullPhoneNumber.charAt(i)
-		if(!allowNumber.includes(n) ) {
+		if(!allowNum.includes(n) ) {
 			return [false, 'Vänligen använd bara siffror'];
 		}
 	}
 	return [true, '']
 }
+//Funktionen för text-rutan
+function isValidMessage(fullMessage) {
+	if(fullMessage.length < 101) {
+		return [false, 'Minst 10 och max 100 tecken tack.'];
+	}
+	const allowSymbol = '0123456789abcdefghijklmnopqristuvwxyzåäö?!,.'
+	for(let i = 0; i < fullMessage.length; i++ ) {
+		let s = fullMessage.charAt(i)
+		if(!allowSymbol.includes(s) ) {
+			return [false, 'Vänligen använd bara bokstäver och siffor']
+		}
+	}
+	return [true, '']
+}
 
-export { isValidFullName, isValidTelephone }
+export { isValidFullName, isValidTelephone, isValidMessage}
